7b8ff7ba609a861553bc2a735c812b7b69cba07a
siddas1991 <65807718+siddas1991@users.noreply.github.com>
2021-06-11 16:23:04 +0530

    Add files via upload

---
+++ Day_2.py
@@ -0,0 +1,7 @@
+GDP =10.5
+Country = 'India'
+Year = 2021
+check = True
+
+print ("The Reserve Bank of India has announced GDP forecast for {} in the year {} to be {}%.".format(Country, Year, GDP))
+print ("This information is verified and is found to be {}".format(check))
